8da2760f832b711c68a61bda977640bb71f36d2d
Andrey Shvydky <andrey@breeze-soft.com>
2013-08-13 17:25:30 +0300

    lazyloader fixes, files renamed to fix MVC bundling

--- src/js/breeze.lazyloader.js
+++ src/js/breeze.lazyloader.js
@@ -4,10 +4,11 @@
 
     var lazyLoader = function (element, options) {
         this.$element = $(element);
-        this.$container = this.$element.find(options.dataContainer);
+        this.$container = options.dataContainer == null ? this.$element : this.$element.find(options.dataContainer);
         this.$loading = this.$element.find(options.loadingSelector);
         this.$loadMore = this.$element.find(options.loadMoreSelector);
         this.options = options;
+        this.active = false;
         this.init();
     }
 
@@ -24,37 +25,52 @@
         reload: function (count) {
             if (typeof count == 'undefined')
                 count = this.options.count;
-            this.$container.empty();
+            $(this.options.dataSelector, this.$element).remove();
+            $(this.options.loadingSelector, this.$element).show();
             this.more(count);
         },
         more: function (count) {
             var plugin = this;
             if (typeof count == 'undefined')
                 count = this.options.count;
-            $.ajax({
-                url: this.options.url,
-                data: $.extend({}, this.options.data, {
-                    start: $(plugin.options.dataSelector, plugin.$element).length,
-                    count: count
-                }),
-                type: this.options.method,
-                success: function (data, status, xhr) {
-                    if (plugin.options.onLoaded != null) plugin.options.onLoaded.call(plugin, plugin.$element, plugin.$container, data, xhr);
-                    plugin.$container.append(data);
-                    plugin.totalCount = xhr.getResponseHeader(plugin.options.totalHeader);
-                    if (plugin.totalCount == $(plugin.options.dataSelector, plugin.$element).length)
-                        plugin.$loading.hide();
-                    if (plugin.options.onAdded != null) plugin.options.onAdded.call(plugin, plugin.$element, plugin.$container, data, xhr);
-                    if (plugin.needMore()) plugin.more(count);
-                }
-            });
+            if (!plugin.active) {
+                plugin.active = true;
+                $.ajax({
+                    url: this.options.url,
+                    data: $.extend({}, this.options.data, {
+                        start: $(plugin.options.dataSelector, plugin.$element).length,
+                        count: count
+                    }),
+                    type: this.options.method,
+                    success: function (data, status, xhr) {
+                        if (plugin.options.onLoaded != null) plugin.options.onLoaded.call(plugin, plugin.$element, plugin.$container, data, xhr);
+                        plugin.$container.append(data);
+                        var totalCount = xhr.getResponseHeader(plugin.options.totalHeader);
+                        if (totalCount != plugin.totalCount) {
+                            plugin.totalCount = totalCount;
+                            if (plugin.options.onTotalCountChanged != null)
+                                plugin.options.onTotalCountChanged.call(plugin, plugin.$element, totalCount);
+                        }
+                        
+                        if (plugin.totalCount <= $(plugin.options.dataSelector, plugin.$element).length)
+                            plugin.$loading.hide();
+                        var force = false;
+                        if (plugin.options.onAdded != null)
+                            force = plugin.options.onAdded.call(plugin, plugin.$element, plugin.$container, data, xhr);
+                        plugin.active = false;
+                        if (force || plugin.needMore()) plugin.more(count);
+                    }
+                });
+            }
         },
         setOption: function (name, value) {
-            throw "NOT IMPLEMENTED";
+            var plugin = this;
+            plugin.options[name] = value;
         },
         needMore: function () {
             var marker = this.$loading;
             return marker.is(':visible') && $(window).scrollTop() + $(window).height() > marker.offset().top;
+                //&& (this.options.autoLoadCount <=0 || this.options.autoLoadCount > $(this.options.dataSelector, this.$element).length);
         }
 
     }
@@ -78,17 +94,19 @@
     }
 
     $.fn.lazyLoader.defaults = {
-        dataContainer: '.data-container',
+        dataContainer: null,
         loadingSelector: '.loading',
         loadMoreSelector: '.load-more',
         dataSelector: '.data-container .data-record',
         count: 10,
+        autoLoadCount: 0,
         totalHeader: 'X-Total-Count',
         url: null,
         method: 'post',
         data: {},
         onLoaded: function (element, container, data, xhr) { },
-        onAdded: function (element, container, data, xhr) { }
+        onAdded: function (element, container, data, xhr) { return false; },
+        onTotalCountChanged: function (element, count) { }
     }
 
     $.fn.lazyLoader.Constructor = lazyLoader
